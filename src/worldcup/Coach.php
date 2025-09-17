5c0c553b6bb61cfa212348ee5f234db12c172453
Maxime Pol Marcet <mapoma@fcbarcelona.cat>
2025-09-17 18:45:48 +0200

    4. Arreglar la clase coach

--- src/worldcup/Coach.php
+++ src/worldcup/Coach.php
@@ -1,24 +1,17 @@
 <?php
-
 namespace WorldCup;
 
 class Coach extends Person {
-    privated $style; 
+    private $style;
 
     public function train() {
         echo "train\n";
     }
 
-    /**
-     * Get the style
-     */
     public function getStyle() {
         return $this->style;
     }
 
-    /**
-     * Set the style
-     */
     public function setStyle($style) {
         $this->style = $style;
     }
