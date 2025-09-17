429b5094f7057c363c498f097836436374b09ba4
Maxime Pol Marcet <mapoma@fcbarcelona.cat>
2025-09-17 18:58:58 +0200

    forwardxgame

--- src/worldcup/Game.php
+++ src/worldcup/Game.php
@@ -1,88 +1,54 @@
 <?php
-
 namespace WorldCup;
 
 use DateTime;
 
+// Importar todas las clases
+require_once __DIR__ . '/Ball.php';
+require_once __DIR__ . '/Person.php';
+require_once __DIR__ . '/Player.php';
+require_once __DIR__ . '/Forward.php';
+require_once __DIR__ . '/Midfielder.php';
+require_once __DIR__ . '/Defender.php';
+require_once __DIR__ . '/Goalkeeper.php';
+require_once __DIR__ . '/Coach.php';
+require_once __DIR__ . '/Team.php';
+require_once __DIR__ . '/Field.php';
+
+// Ejecutar juego
 $game = new Game();
 $game->main();
 
-/**
- * Class to define the game
- */
 class Game {
     public $field;
     public $date;
     public $ball;
     public $teams;
 
-    public function getField() {
-        return $this->field;
-    }
-
-    public function setField($field) {
-        $this->field = $field;
-    }
-
-    public function getDate() {
-        return $this->date;
-    }
-
-    public function setDate($date) {
-        $this->date = $date;
-    }
-
-    public function getBall() {
-        return $this->ball;
-    }
-
-    public function setBall($ball) {
-        $this->ball = $ball;
-    }
-
-    public function getTeams() {
-        return $this->teams;
-    }
-
-    public function setTeams($teams) {
-        $this->teams = $teams;
-    }
+    public function getField() { return $this->field; }
+    public function setField($field) { $this->field = $field; }
+    public function getDate() { return $this->date; }
+    public function setDate($date) { $this->date = $date; }
+    public function getBall() { return $this->ball; }
+    public function setBall($ball) { $this->ball = $ball; }
+    public function getTeams() { return $this->teams; }
+    public function setTeams($teams) { $this->teams = $teams; }
 
     public function main() {
-        echo "starting application\n";
+        echo "\nStarting application\n";
         
         
         $this->setField(new Field(100));
         $this->setDate(new DateTime());
         $this->setBall(new Ball());
 
-        // create players team A
-        $listA = [];
-        $listA[] = new Goalkeeper();
-        $listA[] = new Defender();
-        $listA[] = new Defender();
-        $listA[] = new Defender();
-        $listA[] = new Defender();
-        $listA[] = new Midfielder();
-        $listA[] = new Midfielder();
-        $listA[] = new Midfielder();
-        $listA[] = new Midfielder();
-        $listA[] = new Forward();
-        $listA[] = new Forward();
+        $listA = [new Goalkeeper(), new Defender(), new Defender(), new Defender(), new Defender(),
+                  new Midfielder(), new Midfielder(), new Midfielder(), new Midfielder(),
+                  new Forward(), new Forward()];
 
-        // create players team B
-        $listB = [];
-        $listB[] = new Goalkeeper();
-        $listB[] = new Defender();
-        $listB[] = new Defender();
-        $listB[] = new Defender();
-        $listB[] = new Defender();
-        $listB[] = new Midfielder();
-        $listB[] = new Midfielder();
-        $listB[] = new Midfielder();
-        $listB[] = new Midfielder();
-        $listB[] = new Forward();
-        $listB[] = new Forward();
+        $listB = [new Goalkeeper(), new Defender(), new Defender(), new Defender(), new Defender(),
+                  new Midfielder(), new Midfielder(), new Midfielder(), new Midfielder(),
+                  new Forward(), new Forward()];
 
         $teamA = new Team("NewTeam");
         $teamA->setPlayers($listA);
@@ -92,48 +58,40 @@ class Game {
         $teamB->setPlayers($listB);
         $teamB->setCoach(new Coach());
 
-        $teams = [];
-        $teams[] = $teamA;
-        $teams[] = $teamB;
-        $this->setTeams($teams);
+        $this->setTeams([$teamA, $teamB]);
 
         $this->start();
     }
 
-
     public function start() {
-        echo "starting match actions...\n";
+        echo "starting match actions... \n";
+        echo"  ";
 
         for ($i = 0; $i < 10; $i++) {
-            echo "\n--- Action " . ($i + 1) . " ---\n";
+            echo "\n--- Action \n" . ($i + 1) . " ---\n";
 
-            // select random team
             $teamIndex = array_rand($this->teams);
             $selectedTeam = $this->teams[$teamIndex];
             echo "Team: " . $selectedTeam->getName() . "\n";
 
-            // select random player
             $players = $selectedTeam->getPlayers();
             $playerIndex = array_rand($players);
             $selectedPlayer = $players[$playerIndex];
             echo "Player type: " . (new \ReflectionClass($selectedPlayer))->getShortName() . "\n";
 
-            // common actions
             $selectedPlayer->run();
             $selectedPlayer->passBall();
 
-            // specific actions
             if ($selectedPlayer instanceof Forward) {
                 $selectedPlayer->drible();
                 $selectedPlayer->kick($this->getBall());
-            } else if ($selectedPlayer instanceof Midfielder) {
+            } elseif ($selectedPlayer instanceof Midfielder) {
                 $selectedPlayer->organize();
-            } else if ($selectedPlayer instanceof Defender) {
+            } elseif ($selectedPlayer instanceof Defender) {
                 $selectedPlayer->steal($this->getBall());
-            } else if ($selectedPlayer instanceof Goalkeeper) {
+            } elseif ($selectedPlayer instanceof Goalkeeper) {
                 $selectedPlayer->block($this->getBall());
             }
         }
     }
-    
 }
